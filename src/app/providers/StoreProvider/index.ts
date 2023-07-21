5dcdf737c70ab4c92b6fe2c0b7d3234ef998b14e
Vitaly Baev <vitalij.baev.98@bk.ru>
2023-07-21 23:58:01 +0300

    add async-reducer

--- src/app/providers/StoreProvider/index.ts
+++ src/app/providers/StoreProvider/index.ts
@@ -1,9 +1,10 @@
 import { createReduxStore } from './config/store';
 import { StoreProvider } from './ui/StoreProvider';
-import { StateSchema } from './config/StateSchema';
+import { StateSchema, ReduxStoreWithManager } from './config/StateSchema';
 
 export {
     StoreProvider,
     createReduxStore,
     StateSchema,
+    ReduxStoreWithManager,
 };
